1d96847223aa0e0dda4a131f1d5dec629cada5a2
Damian <damian.k112@o2.pl>
2023-11-02 20:41:34 +0100

    Members files deleted

--- SplitwiseProject/src/main/java/com/splitwiseapp/repository/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-//package com.splitwiseapp.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MemberRepository extends JpaRepository<EventMember, Integer> {
-//}
