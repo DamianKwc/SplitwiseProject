73b8375126d3e7ff1fab06c95417cde3447c1405
Mateusz <mateuszjezierski@wp.eu>
2023-11-04 20:33:51 +0100

    Gitignore added, fix in event editing, code refactor

--- SplitwiseProject/src/main/java/com/splitwiseapp/repository/EventRepository.java
+++ SplitwiseProject/src/main/java/com/splitwiseapp/repository/EventRepository.java
@@ -1,8 +1,10 @@
 package com.splitwiseapp.repository;
 
 import com.splitwiseapp.entity.Event;
+import com.splitwiseapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
     Event findByEventName(String eventName);
+    Event findByOwner(User owner);
 }
